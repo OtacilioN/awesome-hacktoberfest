ed0304c3f2857cf7ac4ef09fef34bc74d2023a03
killer0921 <animesh2002g@gmail.com>
2019-10-27 15:34:56 +0530

    cycle sort using python

--- animesh3.py
+++ animesh3.py
@@ -1,24 +1,53 @@
-# Python program to implement Gnome Sort
+# Python program to impleament cycle sort
 
-# A function to sort the given list using Gnome sort
-def gnomeSort( arr, n):
-	index = 0
-	while index < n:
-		if index == 0:
-			index = index + 1
-		if arr[index] >= arr[index - 1]:
-			index = index + 1
-		else:
-			arr[index], arr[index-1] = arr[index-1], arr[index]
-			index = index - 1
+def cycleSort(array):
+    writes = 0
 
-	return arr
 
-# Driver Code
-arr = [ 34, 2, 10, -9]
+# Loop through the array to find cycles to rotate.
+for cycleStart in range(0, len(array) - 1):
+    item = array[cycleStart]
+
+    # Find where to put the item.
+    pos = cycleStart
+    for i in range(cycleStart + 1, len(array)):
+        if array[i] < item:
+            pos += 1
+
+    # If the item is already there, this is not a cycle.
+    if pos == cycleStart:
+        continue
+
+    # Otherwise, put the item there or right after any duplicates.
+    while item == array[pos]:
+        pos += 1
+    array[pos], item = item, array[pos]
+    writes += 1
+
+    # Rotate the rest of the cycle.
+    while pos != cycleStart:
+
+    # Find where to put the item.
+    pos = cycleStart
+    for i in range(cycleStart + 1, len(array)):
+        if array[i] < item:
+            pos += 1
+
+    # Put the item there or right after any duplicates.
+    while item == array[pos]:
+        pos += 1
+    array[pos], item = item, array[pos]
+    writes += 1
+
+return writes
+
+# driver code
+arr = [1, 8, 3, 9, 10, 10, 2, 4]
 n = len(arr)
+cycleSort(arr)
 
-arr = gnomeSort(arr, n)
-print "Sorted seqquence after applying Gnome Sort :",
-for i in arr:
-	print i,
+print("After sort : ")
+for i in range(0, n):
+    print(arr[i], end = \' \')
+
+    # Code Contributed by Mohit Gupta_OMG <(0_o)>
