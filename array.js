b5956bacd28baa45cc5392794bf15faa7d0b92a7
Garvit parashar <146798675+GARVITPARASHAR@users.noreply.github.com>
2023-10-11 22:03:39 +0530

    Create array.js

---
+++ array.js
@@ -0,0 +1,19 @@
+<script> 
+	// Create and initialize an array 
+	let items = ['GFG', 'Geeks', 'G4G']; 
+
+	// Display the array items 
+	console.log(items); 
+
+	// Create a new empty array 
+	let new_Array = []; 
+
+	// forEach loop to push elements 
+	// into new array 
+	items.forEach(function (item) { 
+		new_Array.push(item); 
+	}); 
+
+	// Display the new array of items 
+	console.log(new_Array); 
+</script>
